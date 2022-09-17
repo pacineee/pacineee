7d85757f8e5dd3442e1ca0858bee4b3356f620a8
pacine <pacifiquenezerwa@gmail.com>
2022-09-17 17:20:38 +0200

    test_php

--- test_php.php
+++ test_php.php
@@ -1,3 +1,20 @@
 <?php
+$fname= $_POST['fname'];
+$lname= $_POST['lname'];
+$Ename = $_POST['email'];
+$description = $_POST['description'];
 
-/>
+//Below is the Database connection
+$conn = new mysqli('localhost','root','','personal');
+if($conn->connect_error){
+    die('Ooops...something goes wrong :'.$conn->connect_error);
+}
+else{
+    $stmt = $conn->prepare("insert into registration(fname, lname, email, description) value(?, ?, ?, ?)");
+    $stmt->bind_param("ssss", $fname, $lname, $email, $description);
+    $stmt->execute();
+    echo "Message delivered...Thanks!";
+    $stm->close();
+    $conn->close();
+}
+?>
